78678df58a5a39c75dce3047af827ffc79f9f610
Viktor Hesselbom <viktor@hesselbom.net>
2023-09-08 14:20:55 +0200

    Revert "Fix optional issue"
    This reverts commit d8f4a274205f6d2ca4357b8b571e967a1e24bc5e.

--- SyncKit/Classes/CoreData/CoreDataAdapter+Private.swift
+++ SyncKit/Classes/CoreData/CoreDataAdapter+Private.swift
@@ -344,7 +344,7 @@ extension CoreDataAdapter {
                               recordID: CKRecord.ID(recordName: entity.identifier!, zoneID: recordZoneID))
         }
         
-        var originalObject: NSManagedObject?
+        var originalObject: NSManagedObject!
         var entityDescription: NSEntityDescription!
         let objectID = self.getObjectIdentifier(for: entity)!
         let entityState = entity.entityState
@@ -352,7 +352,7 @@ extension CoreDataAdapter {
         let changedKeys = entity.changedKeysArray
         
         context.performAndWait {
-            guard let originalObject = self.managedObject(entityName: entityType, identifier: objectID, context: context) else { return }
+            originalObject = self.managedObject(entityName: entityType, identifier: objectID, context: context)
             entityDescription = NSEntityDescription.entity(forEntityName: entityType, in: context)
             let primaryKey = self.identifierFieldName(forEntity: entityType)
             let encryptedFields = self.entityEncryptedFields[entityType]
